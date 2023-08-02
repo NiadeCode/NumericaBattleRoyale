6814d30ce991150a46fa84df36e919620ccbeee6
NiadeCode <jruizramrez@gmail.com>
2023-08-02 17:19:15 +0200

    add theme files

---
+++ src/jvmMain/kotlin/es/niadecode/numericabattleroyale/ui/theme/Colors.kt
@@ -0,0 +1,36 @@
+package es.niadecode.numericabattleroyale.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val Blue500 = Color(0xFF00A8E8)
+val DarkBlue900 = Color(0xFF1D2F6F)
+val Red300 = Color(0xFFF45B69)
+val DarkRed900 = Color(0xFF860914)
+
+/*
+primary = Blue500,
+primaryVariant = DarkBlue900,
+secondary = Red300,
+secondaryVariant = DarkRed900,
+
+background = Color.White,
+surface = Color.White,
+onPrimary = Color.White,
+onSecondary = Color.Black,
+onBackground = Color.Black,
+onSurface = Color.Black,
+*/
+val primary = Blue500
+val primaryVariant = DarkBlue900
+
+val secondary = Red300
+val secondaryVariant = DarkRed900
+
+val accent = Color(0xFF1DD3B0)
+
+
